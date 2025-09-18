56f7501e9f6587267ca41dfcf331535c53254342
lizmanchego <karinamanchego0416@gmail.com>
2025-09-18 10:06:33 -0500

    [ADD] Video Intro

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -29,8 +29,9 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/AeE7lFd9-rM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video. Supervisión labores agrícolas
+
 
 </template>
 
